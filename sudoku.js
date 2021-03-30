7578b379a4794f609e71ca910f34a7b87f11c992
Peter Tempfli <peter.tempfli@deliveryhero.com>
2021-03-30 19:21:08 +0200

    w

--- sudoku.js
+++ sudoku.js
@@ -54,6 +54,15 @@ class Game {
 
 	}
 
+	isSolved() {
+		for (let row = 0; row < 9; row++) {
+			for (let col = 0; col< 9; col++) {
+				if (this.isEmpty(row, col) == true) return false;
+			}
+		}
+		return true;
+	}
+
 	
 	validRow(row) {
 		let whereIsElement = (i, j) => this.grid[i][j];
@@ -164,6 +173,21 @@ class Game {
 		return clones;
 	}
 
+	allNext() {
+		let all = [];
+		let toSolve = [];
+		let ps = this.getNext();
+		console.log('Sum:', ps.length); 
+		
+		let solver = gm => {
+			if (gm.isSolved()) all.push(gm);
+			else gm.allNext(gm);
+		}
+		ps.forEach(solver);
+		console.log('solved:', all.length);
+		return all;
+
+	}
 
 	print() {
 		let i = 0;
@@ -186,6 +210,7 @@ class Game {
 console.log('Hello Sudoku');
 
 let g = new Game();
+/*
 g.setRow(0, [5,3,null,null,          7                                 ]);
 g.setRow(1, [6,null,null,         1, 9, 5]);
 g.setRow(2, [null, 9, 8,          null, null, null,      null, 6]);
@@ -194,7 +219,24 @@ g.setRow(4, [4, null, null,       8, null, 3,            null, null, 1]);
 g.setRow(5, [7, null, null,       null,2, null,          null, null, 6]);
 g.setRow(6, [null, 6, null,       null,null, null,         2, 8,  null]);
 g.setRow(7, [null, null, null,    4, 1, 9,                  null,null, 5]);
-//g.setRow(8, [null, null, null,    null, 8, null,           null, 7, 9]);
+g.setRow(8, [null, null, null,    null, 8, null,           null, 7, 9]);
+*/
+
+g.setRow(0, [5, 3, 4, 6, 7, 8, 9, 1, 2]);
+g.setRow(1, [6,7,2,1,9,5,3,4,8]);
+g.setRow(2, [1,9,8,3,4,2,5,6,7]);
+g.setRow(3, [8,5,9,7,6,1,4,2,3]);
+g.setRow(4, [4,2,6,8,5,3,7,9,1]);
+g.setRow(5, [7,1,3,9,2,4,8,5,6]);
+g.setRow(6, [9,6,1,5,3,7,2,8,4]);
+//g.setRow(7, [2,8,7,4,1,9,6,3,5]);
+g.setRow(7, [2,8,7,4,1,9]);
+g.setRow(8, [3,4,5,2]);
+//g.setRow(8, [3,4,5,2,8,6,1,7,9]);
+//g.setRow(, []);
+
+
+
 
 //g.setNum(0, 8, 5);
 //g.setNum(6, 0, 6);
@@ -205,13 +247,14 @@ g.setRow(7, [null, null, null,    4, 1, 9,                  null,null, 5]);
 //g = new Game();
 //g.setRow(2 ,[1,2,3,4,5,6,7,8]);
 g.print();
-g = g.getFilledClone();
+console.log(g.valid());
+//g = g.getFilledClone();
 g.print();
 console.log('===========================');
-g.getNext().forEach(gm => {
-	console.log('..........................');
-	gm.print();
-	console.log(gm.valid());
+g.allNext().forEach(gm => {
+	//console.log('..........................');
+	//gm.print();
+	console.log(gm.isSolved());
 });
 
 //c = g.getFilledClone();
